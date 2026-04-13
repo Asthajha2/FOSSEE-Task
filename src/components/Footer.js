4f91417e12ecac74aa6e0f6b0a552f8484ad20bd
Asthajha2 <asthakumari393@gmail.com>
2026-04-13 18:11:08 +0530

    Create Footer.js

---
+++ src/components/Footer.js
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <p>Developed by <span>FOSSEE group</span>, IIT Bombay · Empowering open-source education across India</p>
+    </footer>
+  );
+}
